a55abefddbd46a258aef97c4e5494f6bbd973696
jucemehu2020 <mellizohurt@unicauca.edu.co>
2020-10-28 20:48:29 -0500

    jucemehu2020
    Taller-Prueba1

---
+++ src/main/java/co/unicauca/restaurant/orientalApp/Restaurant.java
@@ -0,0 +1,35 @@
+
+package co.unicauca.restaurant.orientalApp;
+
+import co.unicauca.restaurant.services.DishDirector;
+import co.unicauca.restaurant.domain.Dish;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ahurtado
+ */
+public class Restaurant {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char caracter;
+        Scanner tecla = new Scanner(System.in);
+        System.out.println("Desea comida Oriental o Italiana?"); 
+        caracter = tecla.next().charAt(0);
+        if(caracter == 'O'){
+            DishDirector director = new DishDirector(new OrientalDishBuilder());
+            director.create();
+            Dish myDish = director.getDish();
+            System.out.println("El plato le cuesta: "+ myDish.getPrice());    
+        }
+        if(caracter == 'I'){
+            DishDirector director = new DishDirector(new ItalianDishBuilder());
+            director.create();
+            Dish myDish = director.getDish();
+            System.out.println("El plato le cuesta: "+ myDish.getPrice()); 
+        }
+  } 
+}
